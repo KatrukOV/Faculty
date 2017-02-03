7d1521673e5742dad8d1995e9dde30ec19e87c0b
KatrukOV <KatrukOV@gmail.com>
2017-02-03 15:20:03 +0200

    ref SubjectServiceImpl

--- src/main/java/com/katruk/service/impl/SubjectServiceImpl.java
+++ src/main/java/com/katruk/service/impl/SubjectServiceImpl.java
@@ -19,78 +19,52 @@ import java.util.Objects;
 public final class SubjectServiceImpl implements SubjectService {
 
   private final SubjectDao subjectDao;
-  private final TeacherService teacherService;
   private final Logger logger;
 
   public SubjectServiceImpl() {
     this.subjectDao = new SubjectDaoMySql();
-    this.teacherService = new TeacherServiceImpl();
     this.logger = Logger.getLogger(SubjectServiceImpl.class);
   }
 
   @Override
   public Collection<Subject> getAll() throws ServiceException {
-    Collection<Subject> subjects;
     try {
-      subjects = this.subjectDao.getAllSubject();
+      return this.subjectDao.getAllSubject();
     } catch (DaoException e) {
       logger.error("Cannot get all subjects.", e);
       throw new ServiceException("Cannot get all subjects.", e);
     }
-    Collection<Teacher> teachers = this.teacherService.getAll();
-    for (Subject subject : subjects) {
-      teachers.stream()
-          .filter(teacher -> Objects.equals(subject.getTeacher().getId(), teacher.getId()))
-          .forEach(subject::setTeacher);
-    }
-    return subjects;
   }
 
   @Override
   public Subject getSubjectById(final Long subjectId) throws ServiceException {
-    final Subject subject;
     try {
-      subject = this.subjectDao.getSubjectById(subjectId)
-          .orElseThrow(() -> new DaoException("Subject not found", new NoSuchElementException()));
+      return this.subjectDao.getSubjectById(subjectId);
     } catch (DaoException e) {
       logger.error(String.format("Cannot get subject by id: %d.", subjectId), e);
       throw new ServiceException(String.format("Cannot get subject by id: %d.", subjectId), e);
     }
-    final Teacher teacher = this.teacherService.getTeacherById(subject.getTeacher().getId());
-    subject.setTeacher(teacher);
-    return subject;
   }
 
   @Override
   public Collection<Subject> getSubjectByTeacher(final Teacher teacher) throws ServiceException {
-    Collection<Subject> subjects;
     try {
-      subjects = this.subjectDao.getSubjectByTeacher(teacher.getId());
+      return this.subjectDao.getSubjectByTeacher(teacher.getId());
     } catch (DaoException e) {
-      logger.error(
-          String.format("Cannot get subjects by teacher with id: %d.", teacher.getId()), e);
-      throw new ServiceException(
-          String.format("Cannot get subjects by teacher with id: %d.", teacher.getId()), e);
+      String mess = String.format("Cannot get subjects by teacher with id: %d.", teacher.getId());
+      logger.error(mess, e);
+      throw new ServiceException(mess, e);
     }
-    Collection<Teacher> teachers = this.teacherService.getAll();
-    for (Subject subject : subjects) {
-      teachers.stream()
-          .filter(teach -> Objects.equals(subject.getTeacher().getId(), teach.getId()))
-          .forEach(subject::setTeacher);
-    }
-    return subjects;
   }
 
   @Override
   public Subject save(final Subject subject) throws ServiceException {
-    final Subject result;
     try {
-      result = this.subjectDao.save(subject);
+      return this.subjectDao.save(subject);
     } catch (DaoException e) {
       logger.error("Cannot save subject.", e);
       throw new ServiceException("Cannot save subject.", e);
     }
-    return result;
   }
 
   @Override
